5620fa4d7df809a7880845cc57fee6a4682db4de
guihunkun <615815574@qq.com>
2018-11-11 22:27:18 +0800

    Add files via upload

---
+++ GCD.cpp
@@ -0,0 +1,33 @@
+#include"iostream"
+#include"vector" 
+using namespace std; 	
+int gcd(int a,int b)
+{
+	return b?gcd(b,a%b):a;
+}
+//int Gcd(vector<int> cn,int n)
+//{
+// 	int c=cn[0];
+//	for(int i=1;i<n;i++)
+//	{
+//		c=gcd(c,cn[i]);
+//	}
+//	return c;
+//}
+int main()
+{
+// 	int n;
+// 	vector<int> cn;
+// 	cout<<"请输入整数个数"<<endl;
+//	scanf("%d",&n);  
+//	cout<<"请输入n个整数()"<<endl;
+// 	for(int i=0;i<n;i++)
+// 		scanf("%d ",&cn[i]);
+// 	cout<<"他们的最大公约数是："<<endl;
+// 	cout<<gcd(cn,n)<<endl;
+	int a,b;
+	scanf("%d %d",&a,&b);
+	cout<<gcd(a,b)<<endl;
+
+	return 0;
+}
