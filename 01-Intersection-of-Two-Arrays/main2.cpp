f62d1f46cf9fd2b5ea73cdbba44527ad71b3c626
guihunkun <615815574@qq.com>
2019-01-04 08:35:33 +0800

    leetcode

---
+++ 01-Intersection-of-Two-Arrays/main2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+
+// 349. Intersection of Two Arrays
+// https://leetcode.com/problems/intersection-of-two-arrays/description/
+// 时间复杂度: O(nlogn)
+// 空间复杂度: O(n)
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+
+        set<int> record(nums1.begin(), nums1.end());
+
+        set<int> resultSet;
+        for( int i = 0 ; i < nums2.size() ; i ++ )
+            if( record.find(nums2[i]) != record.end() )
+                resultSet.insert( nums2[i] );
+
+        return vector<int>(resultSet.begin(), resultSet.end());
+    }
+};
+
+int main() {
+
+    int nums1[] = {1, 2, 2, 1};
+    vector<int> vec1(nums1, nums1 + sizeof(nums1)/sizeof(int));
+
+    int nums2[] = {2, 2};
+    vector<int> vec2(nums2, nums2 + sizeof(nums2)/sizeof(int));
+
+    vector<int> res = Solution().intersection(vec1, vec2);
+    for(int i = 0 ; i < res.size() ; i ++ )
+        cout<<res[i]<<" ";
+    cout<<endl;
+
+    return 0;
+}
