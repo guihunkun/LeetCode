d52e077cec1c7dac15c0568dd686a2d5fb79109a
guihunkun <615815574@qq.com>
2019-01-13 18:35:00 +0800

    leetcode

---
+++ 01-Reverse-Linked-List/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+// 206. Reverse Linked List
+// https://leetcode.com/problems/reverse-linked-list/description/
+// 时间复杂度: O(n)
+// 空间复杂度: O(1)
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+
+        ListNode* pre = NULL;
+        ListNode* cur = head;
+        while(cur != NULL){
+            ListNode* next = cur->next;
+            cur->next = pre;
+            pre = cur;
+            cur = next;
+        }
+
+        return pre;
+    }
+};
+
+int main() {
+
+    return 0;
+}
