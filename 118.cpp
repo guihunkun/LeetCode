d25a992a491ef34f0f7ac494723b2aee57c9db91
guihun <615815574@qq.com>
2019-04-09 13:48:28 +0800

    leetcode

---
+++ 118.cpp
@@ -0,0 +1,26 @@
+class Solution 
+{
+public:
+    vector<vector<int>> generate(int numRows) 
+    {
+        vector<vector<int>> res;
+        if(numRows==0)
+            return res;
+        if(numRows==1)
+            return {{1}};
+        res = {{1}};
+        vector<int> curr_row;
+        int r=1;
+        while(r < numRows)
+        {
+            r++;
+            curr_row.clear();
+            curr_row.push_back(1);
+            for(int i=1;i<r-1;i++)
+                curr_row.push_back(res[r-2][i-1] + res[r-2][i]);
+            curr_row.push_back(1);
+            res.push_back(curr_row);
+        }
+        return res;
+    }
+};
---
+++ 946.cpp
@@ -0,0 +1,22 @@
+class Solution 
+{
+public:
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) 
+    {
+        int n=pushed.size();
+        if(pushed.size()!=popped.size())
+            return false;
+        stack<int> sta;
+        int j=0;
+        for(int i=0;i<n;i++)
+        {
+            sta.push(pushed[i]);
+            while(!sta.empty() && j<n &&sta.top()==popped[j])
+            {
+                sta.pop();
+                j++;
+            }
+        }
+        return j==n;
+    }
+};
