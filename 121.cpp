08fcf67a153597af9995193bdbb8e8bdc8629b1a
guihun <615815574@qq.com>
2019-04-28 21:47:41 +0800

    leetcode

---
+++ 121.cpp
@@ -0,0 +1,17 @@
+class Solution 
+{
+public:
+    int maxProfit(vector<int>& prices) 
+    {
+        if(prices.size()<=1)
+            return 0;
+        int minprice=INT_MAX;
+        int res=0;
+        for(int i=0;i<prices.size();i++)
+        {
+            minprice=min(minprice,prices[i]);
+            res=max(res,prices[i]-minprice);
+        }
+        return res;
+    }
+};
