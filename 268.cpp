db7255198297ec671a56111c5c56eb81c9bcfc53
guihun <615815574@qq.com>
2019-05-01 08:50:22 +0800

    leetcode

---
+++ 268.cpp
@@ -0,0 +1,37 @@
+/*
+class Solution 
+{
+public:
+    int missingNumber(vector<int>& nums) 
+    {
+        int sumnums=0;
+        for(int i = 0; i < nums.size(); i++)
+            sumnums+=nums[i];
+        return nums.size()*(nums.size()+1)/2-sumnums;
+    }
+};
+*/
+
+class Solution 
+{
+public:
+    int missingNumber(vector<int>& nums) 
+    {
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if(nums[i] == i)
+                continue;
+            else if(nums[i] != i && nums[i]<nums.size())
+            {
+                swap(nums[i],nums[nums[i]]);
+                i--;
+            }
+            else
+                continue;
+        }
+        for(int i = 0; i < nums.size(); i++)
+            if(nums[i] != i)
+                return i;
+        return nums.size();
+    }
+};
