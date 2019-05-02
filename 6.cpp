ddf9b20049824f0780f2bb1815dd745ae3c2e0d7
guihun <615815574@qq.com>
2019-05-02 22:01:41 +0800

    leetcode

---
+++ 6.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        vector<string> vs(numRows, "");
+        int n = s.length(), i = 0;
+        while (i < n) {
+            for (int j = 0; j < numRows && i < n; j++)
+                vs[j].push_back(s[i++]);
+            for (int j = numRows - 2; j >= 1 && i < n; j--)
+                vs[j].push_back(s[i++]);
+        }
+        string zigzag;
+        for (string v : vs) zigzag += v;
+        return zigzag;
+    } 
+};
