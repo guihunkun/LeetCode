1b61875387dffc8e61610e391486a5775b6b6ca4
guihun <615815574@qq.com>
2019-05-08 14:41:38 +0800

    747

---
+++ 747.cpp
@@ -0,0 +1,15 @@
+class Solution 
+{
+public:
+    int dominantIndex(vector<int>& nums) 
+    {
+        int maxnum_Index = 0;
+        for(int i = 0; i < nums.size(); i++)
+            if (nums[i] > nums[maxnum_Index])
+                maxnum_Index = i;
+        for(int i = 0; i < nums.size(); i++)
+            if(maxnum_Index != i && nums[maxnum_Index] < 2* nums[i])
+                return -1;
+        return maxnum_Index;
+    }
+};
