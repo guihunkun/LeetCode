e1781e81a5510091220520bf9b5d700a9c0b5247
guihun <615815574@qq.com>
2019-05-18 23:02:26 +0800

    168

---
+++ 168.cpp
@@ -0,0 +1,17 @@
+class Solution 
+{
+public:
+    string convertToTitle(int n) 
+    {
+        string res;
+        char tmp;
+        while(n)
+        {
+            n --;
+            tmp = 'A' + n % 26;
+            res = tmp + res;
+            n /= 26;
+        }
+        return res;
+    }
+};
