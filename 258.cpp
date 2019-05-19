03fd21401f03fadaf78c83cd05593ff06c928f47
guihun <615815574@qq.com>
2019-05-19 14:01:44 +0800

    258

---
+++ 258.cpp
@@ -0,0 +1,37 @@
+#include"iostream"
+using namespace std;
+class Solution 
+{
+public:
+    int Digits(int num)
+    {
+        int res = 0;
+        while(num != 0)
+        {
+            int dig = (num % 10);
+            res += dig;
+            num = num/10;
+            
+        }
+        return res;
+    }
+    int addDigits(int num) 
+    {
+		int res = 0;
+        while(num >= 10)
+        {
+            
+			res = Digits(num);
+            num = res;
+        }
+        return num;
+    }
+};
+int main()
+{
+	
+	int num = 38;
+	int res = Solution().addDigits(num);
+	cout<<res<<endl;
+	return 0;
+} 
