b9940667707821f2a86ec7ae7970262439e3c044
guihun <615815574@qq.com>
2019-05-26 11:37:22 +0800

    string&int

---
+++ string&int/cmake-build-debug/CMakeFiles/string_int.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/string_int.dir/main.cpp.obj"
+  "string_int.pdb"
+  "string_int.exe"
+  "string_int.exe.manifest"
+  "libstring_int.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/string_int.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
