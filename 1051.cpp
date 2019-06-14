fed3663bd83d69236c08e38422339a98455ed213
guihun <615815574@qq.com>
2019-06-14 09:07:08 +0800

    1051

---
+++ 1051.cpp
@@ -0,0 +1,27 @@
+class Solution 
+{
+public:
+    int heightChecker(vector<int>& heights) 
+    {
+        int res = 0;
+        vector<int> record(101);
+        for(int i = 0; i < heights.size(); i++)
+            record[heights[i]]++;
+        int j =0;
+        for(int i =0; i < record.size() && j < heights.size(); i++)
+        {
+            if(record[i] != 0)
+            {
+                while(record[i] != 0)
+                {
+                    if(heights[j++]!=i)
+                    {
+                        res++;
+                    }
+                    record[i]--;
+                }
+            }
+        }
+        return res;
+    }
+};
