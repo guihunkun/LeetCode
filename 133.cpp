a570ad0f54e8ebd2c9a7a2d4f632a212d97e58f1
guihun <615815574@qq.com>
2019-06-20 21:11:05 +0800

    133

---
+++ 133.cpp
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+class Solution
+{
+public:
+    Node* cloneGraph(Node* node)
+    {
+        if (node == NULL) 
+            return nullptr;
+        unordered_map<Node*, Node*> mmp;
+        queue<Node*> q;
+        q.push(node);
+        // bfs遍历所有节点，设置对应的新节点
+        while(!q.empty())
+        {
+            auto top = q.front();
+            q.pop();
+
+            auto* nod = new Node();
+            nod->val = top->val;
+            mmp[top] = nod;
+            for (const auto n : top->neighbors)
+                if (mmp.find(n) == mmp.end())
+                    q.push(n);
+        }
+        for(auto ite = mmp.begin(); ite != mmp.end(); ++ite)
+        {
+            for(const auto& n : ite->first->neighbors)
+                ite->second->neighbors.push_back(mmp[n]);
+        }
+
+        return mmp[node];
+    }
+};
