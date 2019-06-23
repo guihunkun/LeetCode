f2e1cfed568ccc51be4e36eccb9f61a76e18212b
guihun <615815574@qq.com>
2019-06-23 15:35:31 +0800

    1021

---
+++ 1021.cpp
@@ -0,0 +1,27 @@
+class Solution 
+{
+public:
+    string acc;
+public:
+    string removeOuterParentheses(string S) 
+    {
+        string answer = "";
+        int numOpenParens = 0;
+        for (int i = 0; i < S.length(); i++)
+        {
+            if ( S[i] == '(' )
+            {
+                if (numOpenParens != 0)
+                    answer.push_back('(');
+                numOpenParens += 1;
+            }
+            else
+            {
+                numOpenParens -= 1;
+                if (numOpenParens != 0)
+                    answer.push_back(')');
+            }
+        }
+        return answer;
+    }
+};
