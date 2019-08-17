b293e0bc758a7fe96330d0386edef656364f59cb
guihunkun <615815574@qq.com>
2019-08-17 15:00:58 +0800

    240

---
+++ 240.cpp
@@ -0,0 +1,24 @@
+class Solution 
+{
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) 
+    {
+        if(matrix.size() == 0)
+            return false;
+        if(matrix[0].size() == 0)
+            return false;
+        int m = matrix.size(), n = matrix[0].size();
+        int i = m - 1, j = 0;
+        while(i >= 0 && j < n)
+        {
+            if(matrix[i][j] == target)
+                return true;
+            else if(matrix[i][j] > target)
+                i--;
+            else
+                j++;
+                
+        }
+        return false;  
+    }
+};
