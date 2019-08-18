f53f904cf40c4776aa80ae412f05eea8452e1f35
guihunkun <615815574@qq.com>
2019-08-18 17:26:35 +0800

    162

---
+++ 162.cpp
@@ -0,0 +1,26 @@
+class Solution 
+{
+public:
+    int findPeakElement(vector<int>& nums) 
+    {
+        assert(nums.size()>0);
+        int start=0;
+        int end=nums.size()-1;
+        
+        while(start+1<end)
+        {
+            int mid=start+(end-start)/2;
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])
+                return mid;
+            else if(nums[mid]>nums[mid-1] && nums[mid]<nums[mid+1])
+                start=mid;
+            else
+                end=mid;
+        }
+        if(nums[start] > nums[end]){
+            return start;
+        }
+        return end;
+            
+    }
+};
