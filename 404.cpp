7e6c13ad6c87bd8ec59114e286b82c7dcfd1f78b
guihunkun <615815574@qq.com>
2019-09-15 10:08:59 +0800

    404

---
+++ 404.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution 
+{
+
+public:
+    int sumOfLeftLeaves(TreeNode* root) 
+    {
+        if(root == NULL)
+            return 0;
+        long long sumleft = 0;
+        if(root->left && root->left->left == NULL && root->left->right == NULL)
+        {
+            sumleft += root->left->val;
+        }
+        if(root->left)
+            sumleft += sumOfLeftLeaves(root->left);
+        if(root->right)
+            sumleft += sumOfLeftLeaves(root->right);
+        return sumleft;
+    }
+};
