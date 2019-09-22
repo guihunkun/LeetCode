7102314c3be3377ca02085bc479fadd0226437b7
guihunkun <615815574@qq.com>
2019-09-22 13:21:00 +0800

    540

--- 540.cpp
+++ 540.cpp
@@ -14,3 +14,25 @@ public:
         
     }
 };
+
+
+class Solution 
+{
+public:
+    int singleNonDuplicate(vector<int>& nums) 
+    {
+        if(nums.size() == 1)
+            return nums[0];
+        int left = 0, right = nums.size() - 1;
+        int mid;
+        while(left < right)
+        {
+            mid = left + (right - left)/2;
+            if(((mid%2) && (nums[mid] == nums[mid - 1]))||(((mid%2) == 0) &&(nums[mid] == nums[mid + 1])))
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return nums[right];
+    }
+};
