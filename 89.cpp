a95611295c7aa4619fcabb566c711ab70422cf3e
guihunkun <615815574@qq.com>
2019-10-01 18:19:42 +0800

    89

---
+++ 89.cpp
@@ -0,0 +1,13 @@
+class Solution 
+{
+public:
+    vector<int> grayCode(int n) 
+    {
+        vector<int> res;
+        for(int i = 0; i < 1<<n; i++)
+        {
+            res.push_back(i^i>>1);
+        }
+        return res;
+    }
+};
