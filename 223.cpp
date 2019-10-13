9039250f4d0f5e0f15507cb86b3931157e519e05
guihunkun <615815574@qq.com>
2019-10-13 11:21:26 +0800

    223

---
+++ 223.cpp
@@ -0,0 +1,17 @@
+class Solution 
+{
+public:
+    int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) 
+    {
+        int xRight = min(C,G);
+        int xLeft = max(A,E);
+        int yTop = min(D,H);
+        int yBot = max(F,B);
+        
+        int s = (C-A) * (D-B);
+        if (xRight > xLeft && yTop > yBot)                        
+            s -= (xRight - xLeft)*(yTop - yBot);            
+        s += (G-E) * (H-F);
+        return s;
+    }
+};
