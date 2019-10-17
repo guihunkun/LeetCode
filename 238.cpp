1db0d2cf8c41b6bf06bb512a9307f5c7e17f375b
guihunkun <615815574@qq.com>
2019-10-17 08:33:18 +0800

    238

---
+++ 238.cpp
@@ -0,0 +1,19 @@
+class Solution 
+{
+public:
+    vector<int> productExceptSelf(vector<int>& nums) 
+    {
+        vector<int> res(nums.size(), 1);
+        int right = 1;
+        for(int i = 1; i < nums.size(); i++)
+        {
+            res[i] = res[i-1]*nums[i-1];
+        }
+        for(int i = nums.size() - 2; i >= 0; i--)
+        {
+            right *= nums[i+1];
+            res[i] *= right;
+        }
+        return res;
+    }
+};
