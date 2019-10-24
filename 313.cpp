b03cb41231e70571af898120dea9dcc5e75c246d
guihunkun <615815574@qq.com>
2019-10-24 08:34:47 +0800

    313

---
+++ 313.cpp
@@ -0,0 +1,24 @@
+class Solution 
+{
+public:
+    int nthSuperUglyNumber(int n, vector<int>& primes) 
+    {
+        vector<int> dp(n, 1), idx(primes.size(), 0);
+        for (int i = 1; i < n; ++i) 
+        {
+            dp[i] = INT_MAX;
+            for (int j = 0; j < primes.size(); ++j) 
+            {
+                dp[i] = min(dp[i], dp[idx[j]] * primes[j]);
+            }
+            for (int j = 0; j < primes.size(); ++j) 
+            {
+                if (dp[i] == dp[idx[j]] * primes[j]) 
+                {
+                    ++idx[j];
+                }
+            }
+        }
+        return dp[n-1];
+    }
+};
