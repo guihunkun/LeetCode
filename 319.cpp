ce7abbe5e0f27e13f32b25d6f7a192a055f68875
guihunkun <615815574@qq.com>
2019-10-25 12:46:38 +0800

    319

---
+++ 319.cpp
@@ -0,0 +1,22 @@
+/*
+class Solution 
+{
+public:
+    int bulbSwitch(int n) 
+    {
+        int count = 0;
+        for (int i = 1; i*i <= n; i++) {
+            count++;
+        }
+        return count;
+    }
+};
+*/
+class Solution 
+{
+public:
+    int bulbSwitch(int n) 
+    {
+        return sqrt(n);
+    }
+};
