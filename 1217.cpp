68a707194e2d5d7a424ec79b383d271761725b6e
guihunkun <615815574@qq.com>
2019-11-29 08:25:17 +0800

    1217

---
+++ 1217.cpp
@@ -0,0 +1,20 @@
+class Solution 
+{
+public:
+    int minCostToMoveChips(vector<int>& chips) 
+    {
+        int even = 0, odd = 0;
+        for(int chip : chips)
+        {
+            if((chip&1) == 0)
+            {
+                even++;
+            }
+            else
+            {
+                odd++;
+            }
+        }
+        return min(odd, even);
+    }
+};
