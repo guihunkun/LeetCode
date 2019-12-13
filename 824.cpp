7090d33999105e088b58fae4c5c6c10d421440c1
guihunkun <615815574@qq.com>
2019-12-13 12:47:07 +0800

    824

--- 824.cpp
+++ 824.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution 
 {
 public:
@@ -17,5 +18,71 @@ public:
         }
         ans.pop_back();
         return ans;
+    }
+};
+*/
+class Solution 
+{
+public:
+    /*
+    vector<string> split(const string& s, char ch)
+    {
+        int start = 0;
+        int len = 0;
+        vector<string> ret;
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s[i] == ch)
+            {
+                ret.push_back(s.substr(start,len));
+                start = i + 1;
+                len = 0;
+            }
+            else
+            {
+                len++;
+            }
+        }
+        if(start < s.length())
+            ret.push_back(s.substr(start,len));
+        return ret;
+    }
+    */
+    vector<string> split(const string& s, char delimiter)
+    {
+        vector<string> tokens;
+        string token;
+        istringstream tokenStream(s);
+        while (getline(tokenStream, token, delimiter))
+        {
+            tokens.push_back(token);
+        }
+        return tokens;
+    }
+
+    string toGoatLatin(string S) 
+    {
+        string res;
+        vector<string> tmp = split(S, ' ');
+        for(int i=0;i<tmp.size();i++)
+        {
+            if('a'==tmp[i][0] || 'e'==tmp[i][0] || 'i'==tmp[i][0] || 'o'==tmp[i][0] || 'u'==tmp[i][0] || 'A'==tmp[i][0] || 'E'==tmp[i][0] || 'I'==tmp[i][0] || 'O'==tmp[i][0] || 'U'==tmp[i][0])
+            {
+                res+=tmp[i]+"ma";
+                res.append(i+1,'a');
+            }
+            else
+            {
+                res+=tmp[i].substr(1,tmp[i].size()-1);
+                res+=tmp[i][0];
+                res+="ma";
+                res.append(i+1,'a');
+            }
+            if(i!=tmp.size()-1)
+            {
+                res+=' ';
+            }
+        }
+        return res;
     }
 };
