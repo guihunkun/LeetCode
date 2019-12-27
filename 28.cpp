cfc4722c2159b767c80ed457e162209e1a668716
guihunkun <615815574@qq.com>
2019-12-27 12:34:23 +0800

    28.cpp

---
+++ 28.cpp
@@ -0,0 +1,30 @@
+class Solution 
+{
+public:
+    int strStr(string haystack, string needle) 
+    {
+        if(needle.size() == 0)
+            return 0;
+        if(haystack.size() == 0 || haystack.size() < needle.size())
+            return -1;
+        for(int i = 0; i < haystack.size() - needle.size() + 1; i++)
+        {
+            bool flag = true;
+            if(haystack[i] == needle[0])
+            {
+                for(int j = 0; j < needle.size(); j++)
+                {
+                    if(haystack[i+j] != needle[j])
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag)
+                    return i;
+            }
+        }
+        return -1;
+        
+    }
+};
