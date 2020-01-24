da064d2cef8e9b33fe5ec54cd3f2c6b8e18d88f7
guihunkun <615815574@qq.com>
2020-01-24 08:35:26 +0800

    458

---
+++ 458.cpp
@@ -0,0 +1,9 @@
+class Solution 
+{
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) 
+    {
+        int states = minutesToTest / minutesToDie + 1;
+        return ceil(log(buckets) / log(states));
+    }
+};
