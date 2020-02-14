89dfcd37c4f4bc61082171c036c5a104fce3c499
guihunkun <615815574@qq.com>
2020-02-14 11:04:12 +0800

    109

---
+++ 109.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution 
+{
+public:
+    TreeNode* sortedListToBST(ListNode* head) 
+    {
+        return buildTree(head, nullptr);
+    }
+    TreeNode * buildTree(ListNode* head, ListNode * tail)
+    {
+        if(head == tail) 
+            return nullptr;
+        ListNode* slow = head, *fast = head;
+        while(fast != tail && fast->next != tail)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        TreeNode * root = new TreeNode(slow->val);
+        root->left = buildTree(head, slow);
+        root->right = buildTree(slow->next, tail);
+        return root;
+    }
+};
