c59ec482666532720d54fd44b0a84a6787772bcc
guihunkun <615815574@qq.com>
2020-02-17 08:40:33 +0800

    117

---
+++ 117.cpp
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+class Solution 
+{
+public:
+    Node* connect(Node* root) 
+    {
+        if(!root)
+            return root;
+        queue<Node*> q{{root}};
+        while(!q.empty())
+        {
+            int n=q.size();
+            for(int i=0;i<n;i++)
+            {
+                Node* t=q.front();
+                q.pop();
+                if(i != n-1)
+                {
+                    Node* tNext=q.front();
+                    t->next=tNext;
+                }
+                if(t->left)
+                    q.push(t->left);
+                if(t->right)
+                    q.push(t->right);
+            }
+        }
+        return root;
+    }
+};
