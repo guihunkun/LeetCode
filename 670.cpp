0be9fd16bedc0b6604b52de17bb5f7b84ae05f1f
guihunkun <615815574@qq.com>
2020-05-29 20:52:44 +0800

    670.cpp

---
+++ 670.cpp
@@ -0,0 +1,28 @@
+class Solution 
+{
+public:
+    int maximumSwap(int num) 
+    {
+        string s = to_string(num);
+        vector<int> rightMax(s.size(), 0);
+        int mx = s.size() - 1;
+        for(int i = s.size() - 1; i >= 0; i--) 
+        {
+            if(s[i] > s[mx]) 
+            {
+                mx = i;
+            }
+            rightMax[i] = mx;
+        }
+
+        for(int i = 0; i < s.size(); i++) 
+        {
+            if(s[i] != s[rightMax[i]]) 
+            {
+                swap(s[i], s[rightMax[i]]);
+                break;
+            }
+        }
+        return stoi(s);
+    }
+};
