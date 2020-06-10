e0c75970f20481846c267a50bd1acfb4efa33f1d
guihunkun <615815574@qq.com>
2020-06-10 21:09:07 +0800

    1299

---
+++ 1299.cpp
@@ -0,0 +1,16 @@
+class Solution 
+{
+public:
+    vector<int> replaceElements(vector<int>& arr) 
+    {
+        int maxRight = arr[arr.size() - 1];
+        arr[arr.size() - 1] = -1;
+        for(int i = arr.size()-2; i >= 0; i--)
+        {
+            int num = arr[i];
+            arr[i] = maxRight;
+            maxRight = max(maxRight, num);
+        }
+        return arr;
+    }
+};
