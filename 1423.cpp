f34431f60790e42cb151ad9a05a5d63739ebe45a
guihunkun <615815574@qq.com>
2020-06-25 11:38:20 +0800

    1423

---
+++ 1423.cpp
@@ -0,0 +1,26 @@
+class Solution 
+{
+public:
+    int maxScore(vector<int>& cardPoints, int k) 
+    {
+        int sum = 0;
+        for(auto num : cardPoints)
+        {
+            sum += num;
+        }
+        int n = cardPoints.size();
+        int window = 0;
+        for(int i = 0; i < n - k; i++)
+        {
+            window += cardPoints[i];
+        }
+        int minW = window;
+        for(int i = n-k; i < n; i++)
+        {
+            window += cardPoints[i];
+            window -= cardPoints[i-n+k];
+            minW = min(minW, window);
+        }
+        return sum - minW;
+    }
+};
