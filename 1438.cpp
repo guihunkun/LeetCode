7425a95d44461f38ad16a940274d07747c08dd51
guihunkun <615815574@qq.com>
2020-07-03 21:14:39 +0800

    1438

---
+++ 1438.cpp
@@ -0,0 +1,23 @@
+class Solution 
+{
+public:
+    int longestSubarray(vector<int>& nums, int limit) 
+    {
+        int n = nums.size();
+        int res = 1;
+        int curr = 0;
+        multiset<int> prev;
+        prev.insert(nums[0]);
+        for(int i = 1; i < n; ++i)
+        {
+            prev.insert(nums[i]);
+            while(curr < i && abs(*prev.rbegin()-*prev.begin()) > limit)
+            {
+                prev.erase(nums[curr]);
+                curr++;
+            }
+            res = max(res,(int)prev.size());
+        } 
+        return res;
+    }
+};
