d1f1fe410dee956aaa8f80de908108ebc3275164
guihunkun <615815574@qq.com>
2020-07-11 12:33:50 +0800

    1493

---
+++ 1493.cpp
@@ -0,0 +1,26 @@
+class Solution 
+{
+public:
+    int longestSubarray(vector<int>& nums) 
+    {
+        int left = 0, right = 0;
+        int res = 0, numOfZero = 0;
+        for(; right < nums.size(); right++)
+        {
+            if(nums[right] == 0)
+            {
+                numOfZero++;
+            }
+            while(numOfZero > 1)
+            {
+                if(nums[left] == 0)
+                {
+                    numOfZero--;
+                }
+                left++;
+            }
+            res = max(res, right-left);
+        }
+        return res;
+    }
+};
