03a8b257f8a26eb35cd552d5e83c3b83acd32c59
guihunkun <615815574@qq.com>
2020-07-26 10:04:38 +0800

    1287

---
+++ 1287.cpp
@@ -0,0 +1,24 @@
+class Solution 
+{
+public:
+    int findSpecialInteger(vector<int>& arr) 
+    {
+        int res = arr[0], count = 1;
+        for(int i = 1; i < arr.size(); i++)
+        {
+            if(arr[i] == res)
+            {
+                if(++count > arr.size()/4)
+                {
+                    return res;
+                }
+            }
+            else
+            {
+                res = arr[i];
+                count = 1;
+            }
+        }
+        return res;
+    }
+};
