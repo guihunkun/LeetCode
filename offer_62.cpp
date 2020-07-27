298d5fdd2f5065f562d7efb29a271b8a579067f1
guihunkun <615815574@qq.com>
2020-07-27 22:22:32 +0800

    offer_62

---
+++ offer_62.cpp
@@ -0,0 +1,17 @@
+class Solution 
+{
+public:
+    int f(int n, int m)
+    {
+        if(n == 1)
+        {
+            return 0;
+        }
+        int x = f(n-1, m);
+        return (x+m)%n;
+    }
+    int lastRemaining(int n, int m) 
+    {
+        return f(n, m);
+    }
+};
