f05364e38cd98b0a928f3bf81336dc6a5b824221
guihunkun <615815574@qq.com>
2020-07-27 19:21:32 +0800

    1394

---
+++ 1394.cpp
@@ -0,0 +1,22 @@
+class Solution 
+{
+public:
+    int findLucky(vector<int>& arr) 
+    {
+        int res = -1;
+        map<int, int> rec;
+        for(int i = 0; i < arr.size(); i++)
+        {
+            rec[arr[i]]++;
+        }
+        map<int, int>::iterator iter = rec.begin();
+        for(; iter != rec.end(); iter++)
+        {
+            if((*iter).first == (*iter).second)
+            {
+                res = max(res, (*iter).second);
+            }
+        }
+        return res;
+    }
+};
