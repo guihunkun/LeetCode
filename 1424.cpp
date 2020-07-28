b168b6c0db93c38ec233724d1dadab6adfbcc8d5
guihunkun <615815574@qq.com>
2020-07-28 21:52:12 +0800

    1424

---
+++ 1424.cpp
@@ -0,0 +1,25 @@
+class Solution 
+{
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& nums) 
+    {
+        map<int, vector<int> > rec;
+        for(int i = nums.size()-1; i >= 0; i--)
+        {
+            for(int j = 0; j < nums[i].size(); j++)
+            {
+                rec[i+j].push_back(nums[i][j]);
+            }
+        }
+        vector<int> res;
+        res.reserve(200100);
+        for(auto& rec_iter : rec)
+        {
+            for(auto& num : rec_iter.second)
+            {
+                res.push_back(num);
+            }
+        }
+        return res;
+    }
+};
