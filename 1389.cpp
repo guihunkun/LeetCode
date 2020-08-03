761fb7c658a00236f18070f0b37ab1be08f752d2
guihunkun <615815574@qq.com>
2020-08-03 19:50:12 +0800

    1389

---
+++ 1389.cpp
@@ -0,0 +1,15 @@
+class Solution 
+{
+public:
+    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) 
+    {
+        vector<int> res;
+        vector<int>::iterator it;
+        for(int i = 0; i < index.size(); i++)
+        {
+            it = res.begin() + index[i];
+            res.insert(it, nums[i]);
+        }
+        return res;
+    }
+};
