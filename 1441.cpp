d9eac92f79c05fb8b7c5933e59ec985e558b5a63
guihunkun <615815574@qq.com>
2020-08-26 21:47:18 +0800

    1441

---
+++ 1441.cpp
@@ -0,0 +1,21 @@
+class Solution 
+{
+public:
+    vector<string> buildArray(vector<int>& target, int n) 
+    {
+        vector<string> ans;
+        int nextPush = 1;   // 下一个push的元素   
+        for(int i = 0; i < target.size(); ++i)
+        {
+            while(nextPush<target[i])
+            {
+                ans.push_back("Push");
+                ans.push_back("Pop");
+                nextPush++;
+            }
+            ans.push_back("Push");
+            nextPush++;
+        }
+        return ans;
+    }
+};
