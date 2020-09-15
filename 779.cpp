adf5d6260d3c456580f52dcd99794ae7b9342c85
guihunkun <615815574@qq.com>
2020-09-15 21:17:16 +0800

    779

---
+++ 779.cpp
@@ -0,0 +1,19 @@
+class Solution 
+{
+public:
+    int kthGrammar(int N, int K) 
+    {
+        return helper(N, K, 0);
+    }
+
+    int helper(int N, int K, int flag) 
+    {
+        if(N == 1) return flag;
+        int half = (1 << (N-2));
+        if(K <= half) {
+            return helper(N-1, K, flag);
+        } else {
+            return helper(N-1, K - half, 1-flag);
+        }
+    }
+};
