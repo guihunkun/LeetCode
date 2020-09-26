fb3452b81f779a3f0ceef196b8326edea785de52
guihunkun <615815574@qq.com>
2020-09-26 16:22:36 +0800

    1578

---
+++ 1578.cpp
@@ -0,0 +1,19 @@
+class Solution 
+{
+public:
+    int minCost(string s, vector<int>& cost) 
+    {
+        int n = s.size();
+        int sum = 0;
+        for(int i = 0; i < n-1; i++)
+        {
+            if(s[i] == s[i+1])
+            {
+                sum += min(cost[i], cost[i+1]); 
+                if(cost[i] > cost[i+1])
+                    swap(cost[i], cost[i+1]);
+            }
+        }
+        return sum;
+    }
+};
