0bfc92cb2cb409f747753f723679a8e28904eeac
guihunkun <615815574@qq.com>
2020-09-27 22:11:50 +0800

    1497

---
+++ 1497.cpp
@@ -0,0 +1,21 @@
+class Solution 
+{
+public:
+    bool canArrange(vector<int>& arr, int k) 
+    {
+        unordered_map<int, int> mod;
+        for(int num: arr) 
+        {
+            ++mod[(num % k + k) % k];
+        }
+        for(auto [t, occ]: mod) 
+        {
+            if(t > 0 && (!mod.count(k - t) || mod[k - t] != occ)) 
+            {
+                return false;
+            }
+        }
+        return mod[0] % 2 == 0;
+    }
+};
+
