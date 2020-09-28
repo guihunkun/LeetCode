48524c8586b1ae358e584bd392fd21d506592308
guihunkun <615815574@qq.com>
2020-09-28 21:23:14 +0800

    713

---
+++ 713.cpp
@@ -0,0 +1,22 @@
+class Solution 
+{
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) 
+    {
+        if(k <= 1)
+        {
+            return 0;
+        }
+        int prod = 1, res = 0, left = 0;
+        for(int right = 0; right < nums.size(); right++)
+        {
+            prod *= nums[right];
+            while(prod >= k)
+            {
+                prod /= nums[left++];
+            }
+            res += right-left+1;
+        }
+        return res;
+    }
+};
