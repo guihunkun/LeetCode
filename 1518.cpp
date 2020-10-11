fbaa6f074244bcf08a21ee3b644d7f9015cf2f8f
guihunkun <615815574@qq.com>
2020-10-11 18:03:02 +0800

    1518

---
+++ 1518.cpp
@@ -0,0 +1,15 @@
+class Solution 
+{
+public:
+    int numWaterBottles(int numBottles, int numExchange) 
+    {
+        int bottle = numBottles, res = numBottles;
+        while(bottle >= numExchange) 
+        {
+            bottle -= numExchange;
+            ++res;
+            ++bottle;
+        }
+        return res;
+    }
+};
