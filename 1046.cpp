7e93bbd52f7a78ccddccd355125f362b18807f29
guihunkun <615815574@qq.com>
2020-10-12 21:39:07 +0800

    1046

--- 1046.cpp
+++ 1046.cpp
@@ -3,6 +3,11 @@ class Solution
 public:
     int lastStoneWeight(vector<int>& stones) 
     {
+        /*
+         * 优先队列 : priority_queue
+         * 对于基础类型 默认是大根堆(降序队列) : priority_queue<int> pq;
+         * 小根堆(升序队列) : priority_queue<int, vector<int>, greater<int> > pq; 
+         */
         priority_queue<int> record;
         for(int weight : stones)
             record.push(weight);
