18c360d88d0dd3ffcf0c53b75538f85bf91a8cd6
guihunkun <615815574@qq.com>
2020-11-06 18:13:19 +0800

    1356

---
+++ 1356.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int get(int x){
+        int res = 0;
+        while (x) {
+            res += (x % 2);
+            x /= 2;
+        }
+        return res;
+    }
+    vector<int> sortByBits(vector<int>& arr) {
+        vector<int> bit(10001, 0);
+        for (auto x: arr) {
+            bit[x] = get(x);
+        }
+        sort(arr.begin(),arr.end(),[&](int x,int y){
+            if (bit[x] < bit[y]) {
+                return true;
+            }
+            if (bit[x] > bit[y]) {
+                return false;
+            }
+            return x < y;
+        });
+        return arr;
+    }
+};
+
