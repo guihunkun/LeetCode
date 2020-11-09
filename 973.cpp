4c1e99bd2a5e9f3dae9ee5dc21ae0c35beef98dc
guihunkun <615815574@qq.com>
2020-11-09 20:41:58 +0800

    973

---
+++ 973.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        sort(points.begin(), points.end(), [](const vector<int>& u, const vector<int>& v) {
+            return u[0] * u[0] + u[1] * u[1] < v[0] * v[0] + v[1] * v[1];
+        });
+        return {points.begin(), points.begin() + K};
+    }
+};
+
