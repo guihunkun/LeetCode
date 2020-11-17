7b02d804816f3c83dd3dbdfda1e51246bfa9bb29
guihunkun <615815574@qq.com>
2020-11-17 20:48:24 +0800

    297

---
+++ 297.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ //LeetCode
+class Codec {
+public:
+    string serialize(TreeNode* root) {
+        if (!root) return "X";
+        auto l = "(" + serialize(root->left) + ")";
+        auto r = "(" + serialize(root->right) + ")";
+        return  l + to_string(root->val) + r;
+    }
+
+    inline TreeNode* parseSubtree(const string &data, int &ptr) {
+        ++ptr; // 跳过左括号
+        auto subtree = parse(data, ptr);
+        ++ptr; // 跳过右括号
+        return subtree;
+    }
+
+    inline int parseInt(const string &data, int &ptr) {
+        int x = 0, sgn = 1;
+        if (!isdigit(data[ptr])) {
+            sgn = -1;
+            ++ptr;
+        }
+        while (isdigit(data[ptr])) {
+            x = x * 10 + data[ptr++] - '0';
+        }
+        return x * sgn;
+    }
+
+    TreeNode* parse(const string &data, int &ptr) {
+        if (data[ptr] == 'X') {
+            ++ptr;
+            return nullptr;
+        }
+        auto cur = new TreeNode(0);
+        cur->left = parseSubtree(data, ptr);
+        cur->val = parseInt(data, ptr);
+        cur->right = parseSubtree(data, ptr);
+        return cur;
+    }
+
+    TreeNode* deserialize(string data) {
+        int ptr = 0;
+        return parse(data, ptr);
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser, deser;
+// TreeNode* ans = deser.deserialize(ser.serialize(root));
