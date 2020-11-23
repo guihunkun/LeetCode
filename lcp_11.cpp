6582914272ddce8716f0086255c27e34e985dca4
guihunkun <615815574@qq.com>
2020-11-23 21:05:02 +0800

    lcp_11

---
+++ lcp_11.cpp
@@ -0,0 +1,9 @@
+class Solution 
+{
+public:
+    int expectNumber(vector<int>& scores) 
+    {
+        sort(scores.begin(), scores.end());
+        return unique(scores.begin(), scores.end()) - scores.begin();
+    }
+};
