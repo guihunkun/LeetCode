01a3ecb2d7b1416c94fad32d295de51a3fbf6733
guihunkun <615815574@qq.com>
2020-12-06 11:49:31 +0800

    1550

---
+++ 1550.cpp
@@ -0,0 +1,24 @@
+class Solution 
+{
+public:
+    bool isOdd(const int num)
+    {
+        return num%2 == 1;
+    }
+    bool threeConsecutiveOdds(vector<int>& arr) 
+    {
+        int cnt = 0;
+        for(int i = 0; i < arr.size(); i++)
+        {
+            if(isOdd(arr[i])) {
+                cnt++;
+            } else {
+                cnt = 0;
+            }
+            if(cnt == 3) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
