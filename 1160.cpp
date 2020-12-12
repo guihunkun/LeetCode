0990b84f19787c14dcb4ac1da8715819e63f633d
guihunkun <615815574@qq.com>
2020-12-12 11:33:27 +0800

    1160

---
+++ 1160.cpp
@@ -0,0 +1,33 @@
+class Solution 
+{
+public:
+    int countCharacters(vector<string>& words, string chars) 
+    {
+        int res = 0;
+        map<char, int> rec;
+        for(int i = 0; i < chars.size(); i++)
+        {
+            rec[chars[i]]++;
+        }
+        for(int i = 0; i < words.size(); i++)
+        {
+            string s = words[i];
+            map<char, int> word;
+            for(int j = 0; j < s.size(); j++)
+            {
+                word[s[j]]++;
+            }
+            bool isHave = true;
+            for(int j = 0; j < s.size(); j++)
+            {
+                if(word[s[j]] > rec[s[j]]) {
+                    isHave = false;
+                }
+            }
+            if(isHave) {
+                res += s.size();
+            }
+        }
+        return res;
+    }
+};
