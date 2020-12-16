c24ef1a3515f95c5149eefe28ed6141e37f70970
guihunkun <615815574@qq.com>
2020-12-16 18:42:55 +0800

    1539

---
+++ 1539.cpp
@@ -0,0 +1,23 @@
+class Solution 
+{
+public:
+    int findKthPositive(vector<int>& arr, int k) 
+    {
+        if(arr[0] > k) {
+            return k;
+        }
+        int l = 0, r = arr.size();
+        while(l < r) 
+        {
+            int mid = (l + r) >> 1;
+            int x = mid < arr.size() ? arr[mid] : INT_MAX;
+            if(x - mid - 1 >= k) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return k - (arr[l - 1] - (l - 1) - 1) + arr[l - 1];
+    }
+};
+
