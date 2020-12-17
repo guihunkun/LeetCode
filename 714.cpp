79d77145931310959910538f51a2ceeda4780819
guihunkun <615815574@qq.com>
2020-12-17 09:29:52 +0800

    714

---
+++ 714.cpp
@@ -0,0 +1,17 @@
+class Solution 
+{
+public:
+    int maxProfit(vector<int>& prices, int fee) 
+    {
+        int n = prices.size();
+        vector<vector<int>> dp(n, vector<int>(2));
+        dp[0][0] = 0, dp[0][1] = -prices[0];
+        for (int i = 1; i < n; ++i) 
+        {
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+};
+
