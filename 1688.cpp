8e272ccef12a93c921a2dd21814e17c99d6100fa
guihunkun <615815574@qq.com>
2020-12-18 15:21:44 +0800

    1688

---
+++ 1688.cpp
@@ -0,0 +1,19 @@
+class Solution 
+{
+public:
+    int numberOfMatches(int n) 
+    {
+        int res = 0;
+        while(n > 1)
+        {
+            if(n%2) {
+                res += n/2;
+                n = n/2 + 1;
+            } else {  
+                res += n/2;
+                n = n/2;
+            }
+        }
+        return res;
+    }
+};
