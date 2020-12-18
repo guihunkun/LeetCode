e16373fb3189576fbfc54ed772264884e1e7e6f4
guihunkun <615815574@qq.com>
2020-12-18 10:48:02 +0800

    389

--- 389.cpp
+++ 389.cpp
@@ -1,3 +1,19 @@
+class Solution 
+{
+public:
+    char findTheDifference(string s, string t) 
+    {
+        char c;
+        for(int i = 0; i < s.size(); i++)
+        {
+            c ^= s[i];
+            c ^= t[i];
+        }
+        c ^= t[t.size()-1];
+        return c;
+    }
+};
+/*
 class Solution
 {
 public:
@@ -15,3 +31,4 @@ public:
         return c;
     }
 };
+*/
