3b3f6b7aeb7af75099df77d2f0abcc5274323441
guihunkun <615815574@qq.com>
2020-12-20 09:12:26 +0800

    316

--- 316.cpp
+++ 316.cpp
@@ -3,11 +3,15 @@ public:
     string removeDuplicateLetters(string s) {
         int m[256] = {0}, visited[256] = {0};
         string res = "0";
-        for (auto a : s) ++m[a];
-        for (auto a : s) {
+        for(auto a : s) 
+            ++m[a];
+        for(auto a : s) 
+        {
             --m[a];
-            if (visited[a]) continue;
-            while (a < res.back() && m[res.back()]) {
+            if(visited[a]) 
+                continue;
+            while(a < res.back() && m[res.back()]) 
+            {
                 visited[res.back()] = 0;
                 res.pop_back();
             }
