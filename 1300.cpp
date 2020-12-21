c6627142c1d1eed922e611a9636d7c01a6d86336
guihunkun <615815574@qq.com>
2020-12-21 19:44:10 +0800

    1300

---
+++ 1300.cpp
@@ -0,0 +1,26 @@
+class Solution 
+{
+public:
+    int findBestValue(vector<int>& arr, int target) 
+    {
+        sort(arr.begin(), arr.end());
+        vector<int> pre(arr.size()+1, 0);
+        for( int i = 1; i <= arr.size(); i++)
+        {
+            pre[i] = pre[i-1] + arr[i-1];
+        }
+        int max = *max_element(arr.begin(), arr.end());
+        int res = 0;
+        int diff = target;
+        for(int i = 1; i <= max; i++)
+        {
+            auto iter = lower_bound(arr.begin(), arr.end(), i);
+            int cur = pre[iter - arr.begin()] + (arr.end() - iter)*i;
+            if(abs(cur - target) < diff) {
+                res = i;
+                diff = abs(cur - target);
+            }
+        }
+        return res;
+    }
+};
