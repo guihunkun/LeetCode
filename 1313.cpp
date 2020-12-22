d81c8709ff9857a9db979b056a0d2c6cf3e58934
guihunkun <615815574@qq.com>
2020-12-22 14:57:22 +0800

    1313

---
+++ 1313.cpp
@@ -0,0 +1,18 @@
+class Solution 
+{
+public:
+    vector<int> decompressRLElist(vector<int>& nums) 
+    {
+        vector<int> res;
+        for(int i = 0; i < nums.size()-1; i+=2)
+        {
+            int freq = nums[i];
+            int val = nums[i+1];
+            for(int j = 0; j < freq; j++)
+            {
+                res.push_back(val);
+            }
+        }
+        return res;
+    }
+};
