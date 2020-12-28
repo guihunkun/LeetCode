ff993bd70072d9cbe45b983e41fa3b437619d4ee
guihunkun <615815574@qq.com>
2020-12-28 09:31:22 +0800

    188

---
+++ 188.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        if (prices.empty()) {
+            return 0;
+        }
+
+        int n = prices.size();
+        k = min(k, n / 2);
+        vector<vector<int>> buy(n, vector<int>(k + 1));
+        vector<vector<int>> sell(n, vector<int>(k + 1));
+
+        buy[0][0] = -prices[0];
+        sell[0][0] = 0;
+        for (int i = 1; i <= k; ++i) {
+            buy[0][i] = sell[0][i] = INT_MIN / 2;
+        }
+
+        for (int i = 1; i < n; ++i) {
+            buy[i][0] = max(buy[i - 1][0], sell[i - 1][0] - prices[i]);
+            for (int j = 1; j <= k; ++j) {
+                buy[i][j] = max(buy[i - 1][j], sell[i - 1][j] - prices[i]);
+                sell[i][j] = max(sell[i - 1][j], buy[i - 1][j - 1] + prices[i]);   
+            }
+        }
+
+        return *max_element(sell[n - 1].begin(), sell[n - 1].end());
+    }
+};
+
