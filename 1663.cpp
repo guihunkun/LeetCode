e88ad57d3d35a86a09dc8da4ad1653f4e50558be
guihunkun <615815574@qq.com>
2020-12-29 14:17:05 +0800

    1663

---
+++ 1663.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string getSmallestString(int n, int k) {
+        string ans;
+        for (int rest = n; rest >= 1; --rest) {
+            int bound = k - 26 * (rest - 1);
+            if (bound > 0) {
+                ans += char(bound + 'a' - 1);
+                k -= bound;
+            }
+            else {
+                ans += 'a';
+                k -= 1;
+            }
+        }
+        return ans;
+    }
+};
