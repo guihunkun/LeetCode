a57f5c5a006776fd325db21f9211392357bb2877
guihunkun <615815574@qq.com>
2020-12-30 11:56:26 +0800

    1221

---
+++ 1221.cpp
@@ -0,0 +1,24 @@
+class Solution 
+{
+public:
+    int balancedStringSplit(string s) 
+    {
+        int numR = 0, numL = 0;
+        int res = 0;
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(s[i] == 'L') {
+                numL++;
+            }
+            if(s[i] == 'R') {
+                numR++;
+            }
+            if(numL == numR && numL > 0) {
+                res++;
+                numL = 0;
+                numR = 0;
+            }
+        }
+        return res;
+    }
+};
