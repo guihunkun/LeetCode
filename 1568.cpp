3c2cca3523d2806f964dfa393496c1ba7334e12b
guihunkun <615815574@qq.com>
2021-01-02 12:54:46 +0800

    1568

---
+++ 1568.cpp
@@ -0,0 +1,57 @@
+//leetcode
+class Solution {
+    int dx[4] = {0, 1, 0, -1};
+    int dy[4] = {1, 0, -1, 0};
+public:
+    void dfs(int x, int y, vector<vector<int>>& grid, int n, int m) {
+        grid[x][y] = 2;
+        for (int i = 0; i < 4; ++i) {
+            int tx = dx[i] + x;
+            int ty = dy[i] + y;
+            if (tx < 0 || tx >= n || ty < 0 || ty >= m || grid[tx][ty] != 1) {
+                continue;
+            }
+            dfs(tx, ty, grid, n, m);
+        }
+    }
+    int count(vector<vector<int>>& grid, int n, int m) {
+        int cnt = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] == 1) {
+                    cnt++;
+                    dfs(i, j, grid, n, m);
+                }
+            }
+        }
+        // 还原
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] == 2) {
+                    grid[i][j] = 1;
+                }
+            }
+        }
+        return cnt;
+    }
+    int minDays(vector<vector<int>>& grid) {
+        int n = grid.size(), m = grid[0].size();
+        // 岛屿数量不为 1，陆地已经分离
+        if (count(grid, n, m) != 1) {
+            return 0;
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j]) {
+                    grid[i][j] = 0;
+                    if (count(grid, n, m) != 1) {
+                        // 更改一个陆地单元为水单元后陆地分离
+                        return 1;
+                    }
+                    grid[i][j] = 1;
+                }
+            }
+        }
+        return 2;
+    }
+};
