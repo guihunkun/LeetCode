15958ab9b072e986e130d2187a42a78c742c4a19
guihunkun <615815574@qq.com>
2021-01-13 13:54:47 +0800

    27

---
+++ 27.cpp
@@ -0,0 +1,19 @@
+class Solution 
+{
+public:
+    int removeElement(vector<int>& nums, int val) 
+    {
+        if(nums.empty())    {
+            return 0;
+        }
+        int n = nums.size();
+        int j=0;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            if((nums[i] != val)) {
+                nums[j++] = nums[i];
+            }
+        }
+        return j;
+    }
+};
