0be70dba9cb0f27b15922bfba9c2ed7c4df432a1
guihunkun <615815574@qq.com>
2021-01-14 12:02:58 +0800

    1720

---
+++ 1720.cpp
@@ -0,0 +1,14 @@
+class Solution 
+{
+public:
+    vector<int> decode(vector<int>& encoded, int first) 
+    {
+        vector<int> res(encoded.size()+1);
+        res[0] = first;
+        for(int i = 0; i < encoded.size(); i++)
+        {
+            res[i+1] = res[i] ^ encoded[i]; //Èôa^b=c,Ôòa=b^c
+        }
+        return res;
+    }
+};
