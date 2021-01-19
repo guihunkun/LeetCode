977df65158f065b300f0a6ab8ea6c756d041eceb
guihunkun <615815574@qq.com>
2021-01-19 11:58:17 +0800

    1584

---
+++ 1584.cpp
@@ -0,0 +1,68 @@
+//leetcode
+class DisjointSetUnion {
+private:
+    vector<int> f, rank;
+    int n;
+
+public:
+    DisjointSetUnion(int _n) {
+        n = _n;
+        rank.resize(n, 1);
+        f.resize(n);
+        for (int i = 0; i < n; i++) {
+            f[i] = i;
+        }
+    }
+
+    int find(int x) {
+        return f[x] == x ? x : f[x] = find(f[x]);
+    }
+
+    int unionSet(int x, int y) {
+        int fx = find(x), fy = find(y);
+        if (fx == fy) {
+            return false;
+        }
+        if (rank[fx] < rank[fy]) {
+            swap(fx, fy);
+        }
+        rank[fx] += rank[fy];
+        f[fy] = fx;
+        return true;
+    }
+};
+
+struct Edge {
+    int len, x, y;
+    Edge(int len, int x, int y) : len(len), x(x), y(y) {
+    }
+};
+
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        auto dist = [&](int x, int y) -> int {
+            return abs(points[x][0] - points[y][0]) + abs(points[x][1] - points[y][1]);
+        };
+        int n = points.size();
+        DisjointSetUnion dsu(n);
+        vector<Edge> edges;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                edges.emplace_back(dist(i, j), i, j);
+            }
+        }
+        sort(edges.begin(), edges.end(), [](Edge a, Edge b) -> int { return a.len < b.len; });
+        int ret = 0, num = 1;
+        for (auto& [len, x, y] : edges) {
+            if (dsu.unionSet(x, y)) {
+                ret += len;
+                num++;
+                if (num == n) {
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
+};
