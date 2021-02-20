17957be6f98450415d265765f1e97d1e2f9eb9a0
guihunkun <615815574@qq.com>
2021-02-20 08:37:02 +0800

    1439

---
+++ 1439.cpp
@@ -0,0 +1,23 @@
+class Solution 
+{
+public:
+    int kthSmallest(vector<vector<int>>& mat, int k) 
+    {
+        vector<int> ans(mat[0]);
+        for (int i = 1; i < mat.size(); ++i) {
+            multiset<int> st;
+            for (int x : ans) {
+                for (int y : mat[i]) {
+                    st.insert(x + y);
+                }
+            }
+            ans.assign(st.begin(), st.end());
+            ans.resize(min(k, (int)ans.size()));
+        }
+        return ans.back();
+    }
+};
+
+
+
+
