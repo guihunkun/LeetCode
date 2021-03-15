8038d2120ac87d2e562f25b316f320dc5a00e228
guihunkun <615815574@qq.com>
2021-03-15 09:26:52 +0800

    1110

--- 1110.cpp
+++ 1110.cpp
@@ -19,12 +19,12 @@ public:
 private:
     TreeNode* deleteNode(TreeNode* root, vector<int>& to_delete, vector<TreeNode*>& res, bool no_parent) 
     {
-        if (!root) 
+        if (!root) {
             return root;
+        }
         auto it = find(to_delete.begin(), to_delete.end(), root->val);
         bool td = it != to_delete.end();
-        if (!td && no_parent) 
-        {
+        if (!td && no_parent) {
             res.push_back(root);
         }
         root->left = deleteNode(root->left, to_delete, res, td);
