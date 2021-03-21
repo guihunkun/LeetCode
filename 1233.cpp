93cdebf6fd495d6a0ec906fd17c3f4ad1be752eb
guihunkun <615815574@qq.com>
2021-03-21 14:26:21 +0800

    1233

--- 1233.cpp
+++ 1233.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     vector<string> removeSubfolders(vector<string>& folder)
     {
-        if (folder.empty()) {
+        if(folder.empty()) {
             return{};
         }
         sort(folder.begin(), folder.end());
 
         vector<string> rst(1, folder[0]);
-        for (int i = 1; i < folder.size(); i++) {
-            if (folder[i].compare(0, rst.back().length(), rst.back()) != 0 ||
+        for(int i = 1; i < folder.size(); i++) {
+            if(folder[i].compare(0, rst.back().length(), rst.back()) != 0 ||
                 folder[i][rst.back().length()] != '/') {
                 rst.push_back(folder[i]);
             }
