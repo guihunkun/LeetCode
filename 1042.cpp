ea300c3810eef5261e052ae0d2f6f2932e8f8c88
guihunkun <615815574@qq.com>
2021-03-23 09:10:42 +0800

    1042

--- 1042.cpp
+++ 1042.cpp
@@ -4,13 +4,14 @@ public:
     vector<int> gardenNoAdj(int N, vector<vector<int>>& paths) 
     {
         vector<vector<int>> record(N);
-        for(auto x : paths)
+        for(auto x : paths) {
             record[max(x[0],x[1])-1].push_back(min(x[0],x[1])-1);
+        }
         vector<int> res(N,1); 
-        for (int i = 1; i < N; i++) 
+        for(int i = 1; i < N; i++) 
         {
             set<int> flower{ 1,2,3,4 };
-            for (int j = 0; j < record[i].size(); j++)
+            for(int j = 0; j < record[i].size(); j++)
                 flower.erase(res[record[i][j]]);
             res[i] = *flower.begin();
         }
