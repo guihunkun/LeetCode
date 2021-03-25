0fb65f0c96f9aca5129715aba4f521fb40eb606f
guihunkun <615815574@qq.com>
2021-03-25 00:54:18 +0000

    1208

--- 1208.cpp
+++ 1208.cpp
@@ -4,9 +4,9 @@ class Solution
 public:
     int binarySearch(const vector<int>& accDiff, int endIndex, int target) {
         int low = 0, high = endIndex;
-        while (low < high) {
+        while(low < high) {
             int mid = (high - low) / 2 + low;
-            if (accDiff[mid] < target) {
+            if(accDiff[mid] < target) {
                 low = mid + 1;
             } else {
                 high = mid;
@@ -18,11 +18,11 @@ public:
     int equalSubstring(string s, string t, int maxCost) {
         int n = s.length();
         vector<int> accDiff(n + 1, 0);
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++) {
             accDiff[i + 1] = accDiff[i] + abs(s[i] - t[i]);
         }
         int maxLength = 0;
-        for (int i = 1; i <= n; i++) {
+        for(int i = 1; i <= n; i++) {
             int start = binarySearch(accDiff, i, accDiff[i] - maxCost);
             maxLength = max(maxLength, i - start);
         }
