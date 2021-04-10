8fe19463138c90861029ba551a11df012ab2ef43
guihunkun <615815574@qq.com>
2021-04-10 14:34:35 +0800

    1276

--- 1276.cpp
+++ 1276.cpp
@@ -1,6 +1,8 @@
-class Solution {
+class Solution 
+{
 public:
-    vector<int> numOfBurgers(int tomatoSlices, int cheeseSlices) {
+    vector<int> numOfBurgers(int tomatoSlices, int cheeseSlices) 
+    {
         if (tomatoSlices % 2 != 0 || tomatoSlices < cheeseSlices * 2 || cheeseSlices * 4 < tomatoSlices) {
             return {};
         }
