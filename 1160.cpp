a5a67f7b6c478f7a3407c49de04dbba1a69a8e4d
guihunkun <615815574@qq.com>
2021-04-17 10:52:40 +0800

    1160

--- 1160.cpp
+++ 1160.cpp
@@ -5,21 +5,18 @@ public:
     {
         int res = 0;
         map<char, int> rec;
-        for(int i = 0; i < chars.size(); i++)
-        {
+        for(int i = 0; i < chars.size(); i++) {
             rec[chars[i]]++;
         }
         for(int i = 0; i < words.size(); i++)
         {
             string s = words[i];
             map<char, int> word;
-            for(int j = 0; j < s.size(); j++)
-            {
+            for(int j = 0; j < s.size(); j++) {
                 word[s[j]]++;
             }
             bool isHave = true;
-            for(int j = 0; j < s.size(); j++)
-            {
+            for(int j = 0; j < s.size(); j++) {
                 if(word[s[j]] > rec[s[j]]) {
                     isHave = false;
                 }
