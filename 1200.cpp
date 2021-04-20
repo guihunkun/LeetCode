9c6a8e865d3ac3c51111d5b49ca0f29c8cf2a6d7
guihunkun <615815574@qq.com>
2021-04-20 08:41:10 +0800

    1200

--- 1200.cpp
+++ 1200.cpp
@@ -8,8 +8,7 @@ public:
         sort(arr.begin(), arr.end());
         //找出最小绝对差
         int min_diff = INT_MAX;
-        for(int i = 0; i < arr.size() - 1; ++i)
-        {
+        for(int i = 0; i < arr.size() - 1; ++i) {
             min_diff = std::min(min_diff, arr.at(i + 1) - arr.at(i));
         }
 
@@ -18,8 +17,7 @@ public:
         vv.reserve(arr.size());
         for(int i = 0; i < arr.size() - 1; ++i)
         {
-            if (min_diff == (arr.at(i + 1) - arr.at(i)))
-            {
+            if (min_diff == (arr.at(i + 1) - arr.at(i))) {
                 vv.push_back(vector<int>{arr.at(i), arr.at(i + 1)});
             }
         }
