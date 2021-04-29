80dfe48553283fe749c4283318630c9404ce0a56
guihunkun <615815574@qq.com>
2021-04-29 08:36:44 +0800

    1287

--- 1287.cpp
+++ 1287.cpp
@@ -6,8 +6,7 @@ public:
         int res = arr[0], count = 1;
         for(int i = 1; i < arr.size(); i++)
         {
-            if(arr[i] == res)
-            {
+            if(arr[i] == res) {
                 if(++count > arr.size()/4) {
                     return res;
                 }
