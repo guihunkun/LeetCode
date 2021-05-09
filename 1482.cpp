50d1ff6f36fe576bf9a239f80c7eb14d4dddf834
guihunkun <615815574@qq.com>
2021-05-09 11:00:59 +0800

    1482

---
+++ 1482.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        if (k * m > bloomDay.size()) {
+            return -1;
+        }
+        int low = 1, high = 1;
+        int length = bloomDay.size();
+        for (int i = 0; i < length; i++) {
+            high = max(high, bloomDay[i]);
+        }
+        while (low < high) {
+            int days = (high - low) / 2 + low;
+            if (canMake(bloomDay, days, m, k)) {
+                high = days;
+            } else {
+                low = days + 1;
+            }
+        }
+        return low;
+    }
+
+    bool canMake(vector<int>& bloomDay, int days, int m, int k) {
+        int bouquets = 0;
+        int flowers = 0;
+        int length = bloomDay.size();
+        for (int i = 0; i < length && bouquets < m; i++) {
+            if (bloomDay[i] <= days) {
+                flowers++;
+                if (flowers == k) {
+                    bouquets++;
+                    flowers = 0;
+                }
+            } else {
+                flowers = 0;
+            }
+        }
+        return bouquets >= m;
+    }
+};
