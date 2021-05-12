0932192dff903351a61173582128a9367ce7a6ce
guihunkun <615815574@qq.com>
2021-05-12 08:53:30 +0800

    1310

---
+++ 1310.cpp
@@ -0,0 +1,20 @@
+class Solution 
+{
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) 
+    {
+        int n = arr.size();
+        vector<int> pre_xor(n+1, 0);
+        for(int i = 1; i <= n; i++) {
+            pre_xor[i] = arr[i-1] ^ pre_xor[i-1];
+        }
+        
+        vector<int> ans;
+        for(auto &query : queries) {
+            int L = query[0], R = query[1];
+            ans.push_back(pre_xor[R+1] ^ pre_xor[L]);
+        }
+
+        return ans;
+    }
+};
