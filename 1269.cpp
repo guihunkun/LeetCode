9ef804d62dd5c386ee665086b031eed0e83f30fd
guihunkun <615815574@qq.com>
2021-05-13 08:34:04 +0800

    1269

---
+++ 1269.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    const int MODULO = 1000000007;
+
+    int numWays(int steps, int arrLen) {
+        int maxColumn = min(arrLen - 1, steps);
+        vector<int> dp(maxColumn + 1);
+        dp[0] = 1;
+        for (int i = 1; i <= steps; i++) {
+            vector<int> dpNext(maxColumn + 1);
+            for (int j = 0; j <= maxColumn; j++) {
+                dpNext[j] = dp[j];
+                if (j - 1 >= 0) {
+                    dpNext[j] = (dpNext[j] + dp[j - 1]) % MODULO;
+                }
+                if (j + 1 <= maxColumn) {
+                    dpNext[j] = (dpNext[j] + dp[j + 1]) % MODULO;
+                }
+            }
+            dp = dpNext;
+        }
+        return dp[0];
+    }
+};
