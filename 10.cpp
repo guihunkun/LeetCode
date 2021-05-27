ef907f7b5d3a1ec7280cc0ecbdaf731d8444ecaf
guihunkun <615815574@qq.com>
2021-05-27 09:54:15 +0800

    10

--- 10.cpp
+++ 10.cpp
@@ -16,7 +16,8 @@ public:
 
         vector<vector<int>> f(m + 1, vector<int>(n + 1));
         f[0][0] = true;
-        for (int i = 0; i <= m; ++i) {
+        for (int i = 0; i <= m; ++i) 
+        {
             for (int j = 1; j <= n; ++j) {
                 if (p[j - 1] == '*') {
                     f[i][j] |= f[i][j - 2];
