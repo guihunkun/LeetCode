8beeaa17af62a1f27f21993dcfe8185ca2cec9db
guihunkun <615815574@qq.com>
2021-06-04 08:29:33 +0800

    1030

--- 1030.cpp
+++ 1030.cpp
@@ -6,15 +6,15 @@ public:
         vector<vector<int>> vec;
         multimap<int, pair<int, int>> m;
         for(int i = 0; i<R; i++) {
-        	for(int j = 0; j<C; j++) {
-				int dis = abs(r0 - i) + abs(c0 - j);
-				pair<int, int> par = make_pair(i, j);
-				m.insert({dis, par});
-			}	
-		}
-		for(auto it = m.begin(); it !=m.end(); it++) {
-			vec.push_back({it->second.first, it->second.second});
-		}
-		return vec;
+       	   for(int j = 0; j<C; j++) {
+		int dis = abs(r0 - i) + abs(c0 - j);
+		pair<int, int> par = make_pair(i, j);
+		m.insert({dis, par});
+	   }	
+	}
+	for(auto it = m.begin(); it !=m.end(); it++) {
+	   vec.push_back({it->second.first, it->second.second});
+	}
+	return vec;
     }
 };
