653b999572d9bc8a1059b1a1ad6c6c1b3bb6970b
guihunkun <615815574@qq.com>
2021-06-07 08:31:27 +0800

    1010

--- 1010.cpp
+++ 1010.cpp
@@ -4,9 +4,10 @@ public:
     int numPairsDivisibleBy60(vector<int>& time) 
     {
         int res = 0;
-        vector<int> record(60, 0); // 整除60余数 初始化默认值0
-        for(int i = 0; i < time.size(); i++)
+        vector<int> record(60, 0); // 脮没鲁媒60脫脿脢媒 鲁玫脢录禄炉脛卢脠脧脰碌0
+        for(int i = 0; i < time.size(); i++) {
             record[time[i] % 60]++;
+        }
         res += record[0] * (record[0] - 1) /2 + record[30] * (record[30] - 1) / 2;
         for(int i = 1; i < 30; i++)
             res += record[i] * record[60 - i];
