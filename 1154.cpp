8c5a384ac97b72f182a5b8f0fbf6a4ad9a00d2e2
guihunkun <615815574@qq.com>
2021-06-17 08:01:11 +0800

    1154

--- 1154.cpp
+++ 1154.cpp
@@ -8,8 +8,7 @@ public:
         int month = stoi(date.substr(5, 2));
         int day = stoi(date.substr(8));
         bool runnian = false;
-        if((year%4 == 0 && year%100 != 0) || year%400 == 0 )
-        {
+        if((year%4 == 0 && year%100 != 0) || year%400 == 0 ) {
             runnian = true;
         }
         num += day;
