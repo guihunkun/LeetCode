6d5cd3e3e67f4f0a0fb8ab854ebae58237153e2e
guihunkun <615815574@qq.com>
2021-06-20 08:15:44 +0800

    1600

---
+++ 1600.cpp
@@ -0,0 +1,37 @@
+class ThroneInheritance {
+private:
+    unordered_map<string, vector<string>> edges;
+    unordered_set<string> dead;
+    string king;
+
+public:
+    ThroneInheritance(string kingName): king{move(kingName)} {}
+    
+    void birth(string parentName, string childName) {
+        edges[move(parentName)].push_back(move(childName));
+    }
+    
+    void death(string name) {
+        dead.insert(move(name));
+    }
+    
+    vector<string> getInheritanceOrder() {
+        vector<string> ans;
+
+        function<void(const string&)> preorder = [&](const string& name) {
+            if (!dead.count(name)) {
+                ans.push_back(name);
+            }
+            if (edges.count(name)) {
+                for (const string& childName: edges[name]) {
+                    preorder(childName);
+                }
+            }
+        };
+
+        preorder(king);
+        return ans;
+    }
+};
+
+
