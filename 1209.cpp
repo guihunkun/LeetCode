a8e57dd4da1b8bf6ebe6fd1a98671e6df2426e53
guihunkun <615815574@qq.com>
2021-07-02 08:38:25 +0800

    1209

--- 1209.cpp
+++ 1209.cpp
@@ -8,16 +8,12 @@ public:
         stack<pair<char, int> >st;
         for(long long i = s.size() - 1; i >= 0; i--) 
         {
-            if(!st.empty() && st.top().first == s[i]) 
-            {
+            if(!st.empty() && st.top().first == s[i]) {
                 st.top().second++;
-                if(st.top().second == k) 
-                {
+                if(st.top().second == k) {
                     st.pop();
                 }
-            } 
-            else 
-            {
+            } else {
                 st.push(make_pair(s[i], 1));
             }
         }
