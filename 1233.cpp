c3b847d89f4ded87c1db4d9ab8f969be8bd0bb8a
guihunkun <615815574@qq.com>
2021-07-13 09:28:20 +0800

    1233

--- 1233.cpp
+++ 1233.cpp
@@ -8,7 +8,8 @@ public:
         sort(folder.begin(), folder.end());
 
         vector<string> rst(1, folder[0]);
-        for(int i = 1; i < folder.size(); i++) {
+        for(int i = 1; i < folder.size(); i++) 
+        {
             if(folder[i].compare(0, rst.back().length(), rst.back()) != 0 ||
                 folder[i][rst.back().length()] != '/') {
                 rst.push_back(folder[i]);
