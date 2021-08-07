2364d234790ec57e7ca94cbd6f90e9521815646a
guihunkun <615815574@qq.com>
2021-08-07 12:41:19 +0800

    1290

--- 1290.cpp
+++ 1290.cpp
@@ -12,8 +12,7 @@ public:
     int getDecimalValue(ListNode* head) 
     {
         int res = 0;
-        while(head != NULL)
-        {
+        while(head != NULL) {
             res = res*2 + head->val;
             head = head->next;
         }
