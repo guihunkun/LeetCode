0a52eb47e9f7eb0f96e65a55bad6ddef9bd0ef7c
guihunkun <615815574@qq.com>
2021-08-11 08:57:34 +0800

    122

--- 122.cpp
+++ 122.cpp
@@ -37,8 +37,9 @@ public:
         int maxprofit = 0;
         for(int i=1; i < prices.size(); i++)
         {
-            if(prices[i] > prices[i-1])
+            if(prices[i] > prices[i-1]) {
                 maxprofit += prices[i] - prices[i-1];
+            }
         }
         return maxprofit;
         
