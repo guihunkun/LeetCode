1f9aef74fd0ccc6b1c967642d59c5421aff20478
guihunkun <615815574@qq.com>
2021-08-23 08:49:57 +0800

    1646

---
+++ 1646.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int getMaximumGenerated(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        vector<int> nums(n + 1);
+        nums[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            nums[i] = nums[i / 2] + i % 2 * nums[i / 2 + 1];
+        }
+        return *max_element(nums.begin(), nums.end());
+    }
+};
