385d5f8c0a04bc205af8f7359912da2d2511bc0b
guihunkun <615815574@qq.com>
2021-08-31 09:26:55 +0800

    1109

---
+++ 1109.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
+        vector<int> nums(n);
+        for (auto& booking : bookings) {
+            nums[booking[0] - 1] += booking[2];
+            if (booking[1] < n) {
+                nums[booking[1]] -= booking[2];
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+};
+
