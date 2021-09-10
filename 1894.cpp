a640f40fe38b1f7d16da8222ebae9ca179520334
guihunkun <615815574@qq.com>
2021-09-10 08:43:49 +0800

    1894

---
+++ 1894.cpp
@@ -0,0 +1,21 @@
+class Solution 
+{
+public:
+    int chalkReplacer(vector<int>& chalk, int k) 
+    {
+        int n = chalk.size();
+        if (chalk[0] > k) {
+            return 0;
+        }
+        for (int i = 1; i < n; ++i) 
+        {
+            chalk[i] += chalk[i - 1];
+            if (chalk[i] > k) {
+                return i;
+            }
+        }
+
+        k %= chalk.back();
+        return upper_bound(chalk.begin(), chalk.end(), k) - chalk.begin();
+    }
+};
