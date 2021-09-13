b9da4ed26abbc4cd402324a9b18315846a4075ca
guihunkun <615815574@qq.com>
2021-09-13 09:41:46 +0800

    1232

--- 1232.cpp
+++ 1232.cpp
@@ -9,8 +9,9 @@ public:
         int dy = coordinates[1][1] - coordinates[0][1];
         for(int i = 0; i < coordinates.size() - 1; i++)
         {
-            if(((coordinates[i+1][0] - coordinates[i][0])*dy) != ((coordinates[i+1][1] - coordinates[i][1])*dx))
+            if(((coordinates[i+1][0] - coordinates[i][0])*dy) != ((coordinates[i+1][1] - coordinates[i][1])*dx)) {
                 return false;
+            }
         }
         return true;
     }
