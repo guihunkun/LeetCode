b1098506c0ff44f5830ffe405cf5c6119add9b3d
guihunkun <615815574@qq.com>
2021-09-18 09:41:33 +0800

    1389

--- 1389.cpp
+++ 1389.cpp
@@ -5,7 +5,7 @@ public:
     {
         vector<int> res;
         vector<int>::iterator it;
-        for(int i = 0; i < index.size(); i++)
+        for(int i = 0; i < index.size(); ++i)
         {
             it = res.begin() + index[i];
             res.insert(it, nums[i]);
