5078e775ac6ab9d829a9e7b28c379b97549d46fd
guihunkun <615815574@qq.com>
2021-09-22 08:49:40 +0800

    1503

--- 1503.cpp
+++ 1503.cpp
@@ -4,12 +4,10 @@ public:
     int getLastMoment(int n, vector<int>& left, vector<int>& right) 
     {
         int res = 0;
-        for(int i = 0; i < left.size(); i++)
-        {
+        for(int i = 0; i < left.size(); i++) {
             res = max(res, left[i]);
         }
-        for(int i = 0; i < right.size(); i++)
-        {
+        for(int i = 0; i < right.size(); i++) {
             res = max(res, n-right[i]);
         }
         return res;
