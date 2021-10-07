603f4e26b5da964fdf8250e1f6f291997eac7a55
guihunkun <615815574@qq.com>
2021-10-07 08:15:41 +0800

    1512

--- 1512.cpp
+++ 1512.cpp
@@ -8,8 +8,9 @@ public:
         {
             for(int j = i+1; j < nums.size(); j++)
             {
-                if(nums[i] == nums[j])
+                if(nums[i] == nums[j]) {
                     res++;
+                }
             }
         }
         return res;
