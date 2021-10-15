82bdd37086b470f67c33df5f05eb733dfa4afddc
guihunkun <615815574@qq.com>
2021-10-15 18:02:24 +0800

    1523

--- 1523.cpp
+++ 1523.cpp
@@ -1,10 +1,13 @@
-class Solution {
+class Solution 
+{
 public:
-    int pre(int x) {
+    int pre(int x) 
+    {
         return (x + 1) >> 1;
     }
     
-    int countOdds(int low, int high) {
+    int countOdds(int low, int high)
+    {
         return pre(high) - pre(low - 1);
     }
 };
