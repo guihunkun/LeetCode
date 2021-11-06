58a14eb59d05e34cf5c08bee7ebd27b308336449
guihunkun <615815574@qq.com>
2021-11-06 11:36:32 +0800

    1663

--- 1663.cpp
+++ 1663.cpp
@@ -1,14 +1,16 @@
-class Solution {
+class Solution 
+{
 public:
-    string getSmallestString(int n, int k) {
+    string getSmallestString(int n, int k) 
+    {
         string ans;
-        for (int rest = n; rest >= 1; --rest) {
+        for (int rest = n; rest >= 1; --rest) 
+        {
             int bound = k - 26 * (rest - 1);
             if (bound > 0) {
                 ans += char(bound + 'a' - 1);
                 k -= bound;
-            }
-            else {
+            } else {
                 ans += 'a';
                 k -= 1;
             }
