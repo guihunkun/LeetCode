e655dcc361f5c471e7b306a529f3b3b579631059
guihunkun <615815574@qq.com>
2021-11-18 12:41:20 +0800

    31

--- 31.cpp
+++ 31.cpp
@@ -3,7 +3,7 @@ class Solution
 public:
     void nextPermutation(vector<int>& nums) 
     {
-        for(int i = nums.size() - 2; i >= 0; i--)
+        for(int i = nums.size() - 2; i >= 0; i--) {
             for(int j = nums.size() - 1; j > i; j--)
                 if(nums[i] < nums[j])
                 {
@@ -11,6 +11,7 @@ public:
                     sort(nums.begin() + i + 1, nums.end());
                     return ;
                 }
+        }
         reverse(nums.begin(), nums.end());
     }
 };
