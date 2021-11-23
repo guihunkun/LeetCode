aaf1d11235d81851cf14edec4d643f3ee2ede011
guihunkun <615815574@qq.com>
2021-11-23 09:34:47 +0800

    1478

--- 1478.cpp
+++ 1478.cpp
@@ -1,19 +1,25 @@
-class Solution {
+class Solution 
+{
 public:
-    int minDistance(vector<int>& houses, int K) {
+    int minDistance(vector<int>& houses, int K) 
+    {
         sort(houses.begin(), houses.end());
         int n = houses.size();
         vector<vector<int>> rec(n, vector<int>(n, 0));
-        for(int i = 0; i < n; i ++){
-            for(int j = i; j < n; j ++){
+        for(int i = 0; i < n; i ++)
+        {
+            for(int j = i; j < n; j ++)
+            {
                 int mid = i + j >> 1;
-                for(int k = i; k <= j; k ++){
+                for(int k = i; k <= j; k ++) {
                     rec[i][j] += abs(houses[k] - houses[mid]);
                 }
             }
         }
         vector<vector<int>> dp(n, vector<int>(K + 1, 2e9));
-        for(int i = 0; i < n; i ++) dp[i][1] = rec[0][i];
+        for(int i = 0; i < n; i ++) {
+            dp[i][1] = rec[0][i];
+        }
         for(int i = 0; i < n; i ++){
             for(int j = 2; j <= min(i + 1, K); j ++){
                 for(int k = j - 1; k <= i; k ++){
