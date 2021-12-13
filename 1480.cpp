202ee43d6c0771891c96f1fcb36c1ae9d9f453e9
guihunkun <615815574@qq.com>
2021-12-13 08:33:51 +0800

    1480

--- 1480.cpp
+++ 1480.cpp
@@ -5,8 +5,7 @@ public:
     {
         vector<int> res(nums.size(), 0);
         res[0] = nums[0];
-        for(int i = 1; i < nums.size(); i++)
-        {
+        for(int i = 1; i < nums.size(); i++) {
             res[i] = res[i-1] + nums[i];
         }
         return res;
