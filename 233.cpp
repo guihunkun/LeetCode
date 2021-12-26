0b2ceb71b4b945b6e4fce4879c2af184f26652e1
guihunkun <615815574@qq.com>
2021-12-26 11:46:54 +0800

    233

--- 233.cpp
+++ 233.cpp
@@ -4,8 +4,7 @@ public:
     int countDigitOne(int n) 
     {
         int countr = 0;
-        for(long long i = 1; i <= n; i *= 10) 
-        {
+        for(long long i = 1; i <= n; i *= 10) {
             long long divider = i * 10;
             countr += (n / divider) * i + min(max(n % divider - i + 1, 0LL), i);
         }
