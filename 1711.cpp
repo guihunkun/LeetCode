45584060af183a40e1cfce86607e0ef779efac05
guihunkun <615815574@qq.com>
2021-12-31 09:26:17 +0800

    1711

--- 1711.cpp
+++ 1711.cpp
@@ -1,12 +1,16 @@
 const int MOD = 1e9 + 7;
-class Solution {
+class Solution 
+{
     using LL = long long;
 public:
-    int countPairs(vector<int>& deliciousness) {
+    int countPairs(vector<int>& deliciousness) 
+    {
         unordered_map<int,int> memo;
         LL ans = 0;
-        for (int i = 0;i < deliciousness.size();++i){
-            for (int j = 0;j < 22;++j){
+        for (int i = 0;i < deliciousness.size();++i)
+        {
+            for (int j = 0;j < 22;++j)
+            {
                 int target = pow(2,j);
                 if (target - deliciousness[i] < 0) continue;
                 if (memo.count(target - deliciousness[i])){
