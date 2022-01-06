23dd75add9c24030733bcea8aaf369b60994bc06
guihunkun <615815574@qq.com>
2022-01-06 08:41:14 +0800

    26

--- 26.cpp
+++ 26.cpp
@@ -3,14 +3,15 @@ class Solution
 public:
     int removeDuplicates(vector<int>& nums) 
     {
-        if(nums.size()==0)
+        if(nums.size()==0) {
             return 0;
+        }
         int j=0;
         for(int i=1;i<nums.size();i++)
         {
-            if(nums[j]!=nums[i])
+            if(nums[j]!=nums[i]) {
                 nums[++j]=nums[i];
-                
+            }
         }
         return j+1;
     }
