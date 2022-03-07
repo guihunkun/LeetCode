2fb4c3f7c455537f1b56ffb24182f0b0f0e99aa3
guihunkun <615815574@qq.com>
2022-03-07 09:03:04 +0800

    330

--- 330.cpp
+++ 330.cpp
@@ -43,5 +43,3 @@ public:
 /*
 定义整数bound，[0,bound)是现在能满足的区间，即[0,bound)内的数字都能被表示出来。数列中往后读一个数num，如果num<=bound，那么我们可以扩展区间为[0,bound+num)；如果num>bound，那么我们就需要添加一个数字来扩展区间，最有效的值显然为bound本身，区间扩展为[0,bound+bound)。定义bound初始值为1，循环读取数列中的数字并扩展区间，直到bound>n即可。
 */
-
-
