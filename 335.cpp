c36411d9ce43a710661eccff0cf5355d95247628
guihunkun <615815574@qq.com>
2022-03-10 08:30:20 +0800

    335

--- 335.cpp
+++ 335.cpp
@@ -10,13 +10,11 @@ public:
         {
             if(x[i-3] >= x[i-1] && x[i-2] <= x[i])
                 return true;
-            if(i > 3)
-            {
+            if(i > 3) {
                 if(x[i-4]+x[i] >= x[i-2] && x[i-3] == x[i-1])
                     return true;
             }
-            if(i>4)
-            {
+            if(i>4) {
                 if(x[i]+x[i-4]>=x[i-2] && x[i-2]>=x[i-4] && x[i-5]+x[i-1]>=x[i-3] && x[i-1]<=x[i-3])
                     return true;
             }
