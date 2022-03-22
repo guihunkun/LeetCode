a9edaba5b32eb10451e0d8f1f0bc1c863aa76851
guihunkun <615815574@qq.com>
2022-03-22 08:28:24 +0800

    367

--- 367.cpp
+++ 367.cpp
@@ -4,8 +4,9 @@ public:
     bool isPerfectSquare(int num) 
     {
         long long r = num / 2 + 1;
-        while(r * r > num)
+        while(r * r > num) {
             r = (r + num / r) / 2;
+        }
         return r *r == num;
     }
 };
