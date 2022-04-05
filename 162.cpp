88f79fc0e26e3c6d2e0007f13c969ab3b4b05e9a
guihunkun <615815574@qq.com>
2022-04-05 14:18:26 +0800

    162

--- 162.cpp
+++ 162.cpp
@@ -6,7 +6,6 @@ public:
         assert(nums.size()>0);
         int start=0;
         int end=nums.size()-1;
-        
         while(start+1<end)
         {
             int mid=start+(end-start)/2;
