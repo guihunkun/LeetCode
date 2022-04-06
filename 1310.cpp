942f53cc7cfe04dce3c6bf7bd8179250013dc2dc
guihunkun <615815574@qq.com>
2022-04-06 08:02:19 +0800

    1310

--- 1310.cpp
+++ 1310.cpp
@@ -8,13 +8,11 @@ public:
         for(int i = 1; i <= n; i++) {
             pre_xor[i] = arr[i-1] ^ pre_xor[i-1];
         }
-        
         vector<int> ans;
         for(auto &query : queries) {
             int L = query[0], R = query[1];
             ans.push_back(pre_xor[R+1] ^ pre_xor[L]);
         }
-
         return ans;
     }
 };
